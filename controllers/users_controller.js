3d1eda7ed56b9e8b76f9356e6633076bb5af3210
vaibhavghaytadkar <vghaytadkar@gmail.com>
2022-06-20 21:08:38 +0530

    created controller for sign up page and added routes for the same

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/user');
+
+
 module.exports.profile=function(req,res){
     return res.end('<h1>User Profile accessed</h1>');
 }
@@ -18,5 +21,30 @@ module.exports.signIn =function(req,res){
 
 //get the Sign up Data
 module.exports.create=function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email : req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding user in Signing up');
+            return ;
+        }
+        //when user is not there create user
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log("Error in creating user while signing up");
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+}
+
+//Sign in and create a session for users
+module.exports.createSession = function(req,res){
     
 }
