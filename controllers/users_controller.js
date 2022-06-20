dec9646f2f8dc9db1ece2a60dc0283434fed95bd
vaibhavghaytadkar <vghaytadkar@gmail.com>
2022-06-20 18:21:39 +0530

    set user Schema and created form for sign in /up and created alternate cookie

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,22 @@
 module.exports.profile=function(req,res){
     return res.end('<h1>User Profile accessed</h1>');
 }
+
+//Render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+
+//Render the sign in page
+module.exports.signIn =function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
+
+//get the Sign up Data
+module.exports.create=function(req,res){
+    
+}
